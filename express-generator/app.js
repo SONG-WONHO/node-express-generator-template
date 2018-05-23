af9975f2bfe59ff9310887c148a078aec940384f
SONG-WONHO <win778500@gmail.com>
2018-05-23 15:44:18 +0900

    Add files via upload

---
+++ express-generator/app.js
@@ -0,0 +1,45 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//middleware - logger
+app.use(logger('dev'));
+//middleware - body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+//middleware - cookie-parser
+app.use(cookieParser());
+//middleware - static
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+//catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+//middleware - error-handler
+app.use((err, req, res) => {
+  //set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+
+  //render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
