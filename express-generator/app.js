475d42b79fc7616b65a6feeaf593a759ced76719
SONG-WONHO <win778500@gmail.com>
2018-05-24 11:29:04 +0900

    Update app.js

--- express-generator/app.js
+++ express-generator/app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 });
 
 //middleware - error-handler
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   //set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
